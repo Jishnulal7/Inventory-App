98048b8d4c756b82a47b0c818e71280d37e25b40
jishnulal7 <Jishnulal9497@gmail.com>
2023-07-27 14:24:32 +0530

    Added SignUp Screen

--- lib/screens/user/signup_screen.dart
+++ lib/screens/user/signup_screen.dart
@@ -1,8 +1,12 @@
+// ignore_for_file: avoid_print
+
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-
 import '../home/homescreen.dart';
 import 'signin_screen.dart';
+import 'package:http/http.dart' as http;
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -30,7 +34,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
           },
         ),
       );
-    }else {
+    } else {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(
           content: Text('Please enter the details'),
@@ -39,15 +43,34 @@ class _SignUpScreenState extends State<SignUpScreen> {
     }
   }
 
+  Future<void> register() async {
+    var url = Uri.parse("http://192.168.176.126/inventory/register.php");
+    var response = await http.post(url, body: {
+      "email": _emailController.text,
+      "password": _passwordController.text,
+    });
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      if (data['success'] == true) {
+        print('Registration successful');
+      } else {
+        print('Registration failed');
+      }
+    }
+    else {
+      print('Error: ${response.statusCode}');
+    }
+  }
+
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-  final _confirmPasswordController = TextEditingController();
 
   @override
   void dispose() {
     _passwordController.dispose();
     _emailController.dispose();
-    _confirmPasswordController.dispose();
+
     super.dispose();
   }
 
@@ -151,52 +174,8 @@ class _SignUpScreenState extends State<SignUpScreen> {
             const SizedBox(
               height: 16,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24),
-              child: TextFormField(
-                validator: (value) {
-                  if (value!.isEmpty) {
-                    return 'Enter a valid password!';
-                  }
-                  if (value.length < 8) {
-                    return 'Password must be at least 8 characters long.';
-                  }
-                  if (!value.contains(RegExp(r'[A-Z]'))) {
-                    return 'Password must contain at least one uppercase letter.';
-                  }
-                  return null;
-                },
-                controller: _confirmPasswordController,
-                obscureText: obscureText2,
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  filled: true,
-                  fillColor: const Color(0xFFF5F5F5),
-                  contentPadding: const EdgeInsets.all(20),
-                  hintText: 'Confirm Password',
-                  suffixIcon: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        obscureText2 = !obscureText2;
-                      });
-                    },
-                    child: Icon(
-                      obscureText2 ? Icons.visibility : Icons.visibility_off,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                  ),
-                  prefixIcon: Icon(
-                    CupertinoIcons.lock,
-                    color: Theme.of(context).primaryColor,
-                  ),
-                ),
-              ),
-            ),
-            const SizedBox(
-              height: 16,
-            ),
             ElevatedButton(
-              onPressed: checkLogin,
+              onPressed: register,
               child: const Text(
                 'Sign Up',
                 style: TextStyle(
