66a57e3931c92878e9b7ef210d43927e27d99eed
jishnulal7 <Jishnulal9497@gmail.com>
2023-07-27 14:23:59 +0530

    Added Themes

--- lib/utils/themes.dart
+++ lib/utils/themes.dart
@@ -5,7 +5,7 @@ ThemeData theme() {
   return ThemeData(
     fontFamily: GoogleFonts.lato().fontFamily,
     primaryColor: const Color.fromRGBO(103, 58, 183, 1),
-    elevatedButtonTheme:  const ElevatedButtonThemeData(
+    elevatedButtonTheme: const ElevatedButtonThemeData(
       style: ButtonStyle(
         backgroundColor: MaterialStatePropertyAll(
           Color.fromRGBO(103, 58, 183, 1),
@@ -15,5 +15,19 @@ ThemeData theme() {
         ),
       ),
     ),
+    appBarTheme: const AppBarTheme(
+      elevation: 0,
+      backgroundColor: Colors.deepPurple,
+    ),
+    textTheme: const TextTheme(
+        bodyMedium: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20,
+        ),
+        bodySmall: TextStyle(
+          color: Colors.white,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        )),
   );
 }
