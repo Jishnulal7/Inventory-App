bb0f2f57b1f270bb70dd3e715ec84fb141ce04d1
jishnulal7 <Jishnulal9497@gmail.com>
2023-07-28 15:49:41 +0530

    Added Complete Order Screen

---
+++ lib/screens/product/complete_order_product.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:inventory_app/bloc/bloc_exports.dart';
+import 'package:inventory_app/widgets/product_listing.dart';
+
+import '../../models/product_model.dart';
+
+class CompletedProductScreen extends StatelessWidget {
+  const CompletedProductScreen({super.key});
+  static const id = 'product_screen';
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<ProductBloc, ProductState>(
+      builder: (context, state) {
+        List<Product> productList = state.completedProducts;
+        return Column(
+          children: [
+            const SizedBox(
+              height: 10,
+            ),
+            Center(
+              child: Chip(
+                  label: Text('${productList.length} Products')),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Expanded(
+              child: ProductList(productList: productList),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
